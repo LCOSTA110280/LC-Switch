7fce8cdaac9452a5de87013a752282a4912a6ee5
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-01-30 15:36:06 -0300

    Feito!!!

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, StyleSheet,Switch } from 'react-native';
 
-export default function App() {
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      status :false,
+    }
+    
+  }
+
+  render(){  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Switch
+        value={this.state.status}
+        onValueChange={(valorSwitch)=>this.setState({status:valorSwitch})}
+        thumbColor={'green'}
+      >
+      </Switch>
+      <Text>{(this.state.status)?"Ativo":"Inativo"}</Text>
+
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    marginTop:20,
   },
-});
+  
+    
+})
+
+export default App;
+
+
